d27dfe7df3ab4042537b62ba12d08cd2703ed8c9
Parag <paragspatil123@gmail.com>
2019-10-11 15:27:27 +0530

    day 8 morning challage complete

--- bin/main.dart
+++ bin/main.dart
@@ -11,8 +11,8 @@ void main() {
   List<List<String>> board = createBoard();
   printBoard(board);
   String currentPlayer = 'X';
-
-  while (true) {
+bool flag = true;
+  while (flag) {
     print('$currentPlayer\'s move');
     String userChoice = stdin.readLineSync();
     int row = getRow(userChoice);
@@ -28,6 +28,41 @@ void main() {
     } else {
       currentPlayer = 'X';
     }
+    if(board[0][0]== board[0][1] && board[0][0]== board[0][2] && board[0][0]=='X' || board[0][0]== board[0][1] && board[0][0]== board[0][2] && board[0][0]=='O'  ){
+      String w  =  board[0][0];
+      print('$w won this round');
+      flag = false;
+    }//row1
+    if(board[1][0]== board[1][1] && board[1][0]== board[1][2] && board[1][0] == 'X' || board[1][0]== board[1][1] && board[1][0]== board[1][2] && board[1][0] == 'O'  ){
+      String w  =  board[1][0];
+      print('$w won this round');
+      flag = false;
+    }//row2
+    if(board[2][0]== board[2][1] && board[2][0]== board[2][2] && board[2][0] == 'X' || board[2][0]== board[2][1] && board[2][0]== board[2][2] && board[2][0] == 'O'  ){
+      String w  =  board[2][0];
+      print('$w won this round');
+      flag = false;
+    }//row3
+    if(board[0][0]== board[1][0] && board[0][0]== board[2][0] && board[0][0] == 'X' || board[0][0]== board[1][0] && board[0][0]== board[2][0] && board[0][0] == 'O' ){
+      String w  =  board[0][0];
+      print('$w won this round');
+      flag = false;
+    }//column1
+    if(board[0][1]== board[1][1] && board[2][1]== board[1][1] && board[0][1] =='X' || board[0][1]== board[1][1] && board[2][1]== board[1][1] && board[0][1] =='O' ){
+      String w  =  board[0][1];
+      print('$w won this round');
+      flag = false;
+    }//column2
+    if(board[0][2]== board[1][2] && board[0][2]== board[2][2] && board[0][2] =='X' || board[0][2]== board[1][2] && board[0][2]== board[2][2] && board[0][2] =='O'   ){
+      String w  =  board[0][2];
+      print('$w won this round');
+      flag = false;
+    }//column3
+    if(board[1][1]== board[2][2] && board[0][0]== board[1][1] && board[1][1]== 'X' || board[1][1]== board[2][2] && board[0][0]== board[1][1] && board[1][1]== 'X' ){
+      String w  =  board[0][0];
+      print('$w won this round');
+      flag = false;
+    }//diagonal
   }
 }
 
---
+++ bin/test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import 'main.dart';
+
+void main(){
+  test('Board size teasting', (){
+    List<List<String>> testboard = createBoard();
+    expect(testboard.length, 3);
+    expect(testboard[0].length, 3);
+    expect(testboard[1].length, 3);
+    expect(testboard[2].length, 3);
+    expect(testboard[1][1], ' ');
+
+  });
+
+
+}
